2a5a3ecd14ec844b9e84112e9a31692b87f9760d
Alexander Wessels <alexwessels@yahoo.de>
2023-12-02 12:48:54 +0100

    initial API

---
+++ src/main/java/com/example/restapih2/model/User.java
@@ -0,0 +1,66 @@
+package com.example.restapih2.model;
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "vorname")
+    private String vorname;
+
+    @Column(name = "email", unique = true)
+    private String email;
+
+    public User() {
+    }
+    public User(String name, String vorname, String email) {
+        this.name = name;
+        this.vorname = vorname;
+        this.email = email;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getVorname() {
+        return vorname;
+    }
+
+    public void setVorname(String vorname) {
+        this.vorname = vorname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "User [id=" + id + ", name=" + name + ", vorname=" + vorname + ", email=" + email + "]";
+    }
+
+}
---
+++ src/main/java/com/example/restapih2/repository/UserRepository.java
@@ -0,0 +1,14 @@
+package com.example.restapih2.repository;
+
+import java.util.List;
+
+import com.example.restapih2.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    List<User> findByName(String name);
+
+    List<User> findByVorname(String vorname);
+
+    List<User> findByEmail(String email);
+}
